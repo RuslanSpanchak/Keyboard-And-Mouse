ce38c2ec2b6423c6bda6ae7be0f74cef3e5c7b96
Ruslan Spanchak <105106008+RuslanSpanchak@users.noreply.github.com>
2022-06-18 14:02:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,533 @@
+$(".mouse").hide(0)
+$("#btn_difference").html("Try mouse")
+
+
+document.body.onkeydown = function(e) {
+
+	if (e.keyCode === 49) {
+
+		btn1.style.boxShadow = "0px 0px 5px #555"
+		btn1.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 50) {
+
+		btn2.style.boxShadow = "0px 0px 5px #555"
+		btn2.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 51) {
+
+		btn3.style.boxShadow = "0px 0px 5px #555"
+		btn3.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 52) {
+
+		btn4.style.boxShadow = "0px 0px 5px #555"
+		btn4.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 53) {
+
+		btn5.style.boxShadow = "0px 0px 5px #555"
+		btn5.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 54) {
+
+		btn6.style.boxShadow = "0px 0px 5px #555"
+		btn6.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 55) {
+
+		btn7.style.boxShadow = "0px 0px 5px #555"
+		btn7.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 56) {
+
+		btn8.style.boxShadow = "0px 0px 5px #555"
+		btn8.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 57) {
+
+		btn9.style.boxShadow = "0px 0px 5px #555"
+		btn9.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 48) {
+
+		btn0.style.boxShadow = "0px 0px 5px #555"
+		btn0.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 8) {
+
+		btnBackspace.style.boxShadow = "0px 0px 5px #555"
+		btnBackspace.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 9) {
+
+		btnTab.style.boxShadow = "0px 0px 5px #555"
+		btnTab.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 81) {
+
+		btnQ.style.boxShadow = "0px 0px 5px #555"
+		btnQ.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 87) {
+
+		btnW.style.boxShadow = "0px 0px 5px #555"
+		btnW.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 69) {
+
+		btnE.style.boxShadow = "0px 0px 5px #555"
+		btnE.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 82) {
+
+		btnR.style.boxShadow = "0px 0px 5px #555"
+		btnR.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 84) {
+
+		btnT.style.boxShadow = "0px 0px 5px #555"
+		btnT.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 89) {
+
+		btnY.style.boxShadow = "0px 0px 5px #555"
+		btnY.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 85) {
+
+		btnU.style.boxShadow = "0px 0px 5px #555"
+		btnU.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 73) {
+
+		btnI.style.boxShadow = "0px 0px 5px #555"
+		btnI.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 79) {
+
+		btnO.style.boxShadow = "0px 0px 5px #555"
+		btnO.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 80) {
+
+		btnP.style.boxShadow = "0px 0px 5px #555"
+		btnP.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 65) {
+
+		btnA.style.boxShadow = "0px 0px 5px #555"
+		btnA.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 83) {
+
+		btnS.style.boxShadow = "0px 0px 5px #555"
+		btnS.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 68) {
+
+		btnD.style.boxShadow = "0px 0px 5px #555"
+		btnD.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 70) {
+
+		btnF.style.boxShadow = "0px 0px 5px #555"
+		btnF.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 71) {
+
+		btnG.style.boxShadow = "0px 0px 5px #555"
+		btnG.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 72) {
+
+		btnH.style.boxShadow = "0px 0px 5px #555"
+		btnH.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 74) {
+
+		btnJ.style.boxShadow = "0px 0px 5px #555"
+		btnJ.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 75) {
+
+		btnK.style.boxShadow = "0px 0px 5px #555"
+		btnK.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 76) {
+
+		btnL.style.boxShadow = "0px 0px 5px #555"
+		btnL.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 13) {
+
+		btnenter.style.boxShadow = "0px 0px 5px #555"
+		btnenter.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 16) {
+
+		btnShift.style.boxShadow = "0px 0px 5px #555"
+		btnShift.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 90) {
+
+		btnZ.style.boxShadow = "0px 0px 5px #555"
+		btnZ.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 88) {
+
+		btnX.style.boxShadow = "0px 0px 5px #555"
+		btnX.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 67) {
+
+		btnC.style.boxShadow = "0px 0px 5px #555"
+		btnC.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 86) {
+
+		btnV.style.boxShadow = "0px 0px 5px #555"
+		btnV.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 66) {
+
+		btnB.style.boxShadow = "0px 0px 5px #555"
+		btnB.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 78) {
+
+		btnN.style.boxShadow = "0px 0px 5px #555"
+		btnN.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 77) {
+
+		btnM.style.boxShadow = "0px 0px 5px #555"
+		btnM.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 17) {
+
+		btnCtrl.style.boxShadow = "0px 0px 5px #555"
+		btnCtrl.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 18) {
+
+		btnAlt.style.boxShadow = "0px 0px 5px #555"
+		btnAlt.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 32) {
+
+		btnspace.style.boxShadow = "0px 0px 5px #555"
+		btnspace.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 37) {
+
+		btnleft.style.boxShadow = "0px 0px 5px #555"
+		btnleft.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 38) {
+
+		btntop.style.boxShadow = "0px 0px 5px #555"
+		btntop.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 40) {
+
+		btndown.style.boxShadow = "0px 0px 5px #555"
+		btndown.style.backgroundColor = "darkgrey"
+
+	} else if (e.keyCode === 39) {
+
+		btnright.style.boxShadow = "0px 0px 5px #555"
+		btnright.style.backgroundColor = "darkgrey"
+
+	}
+
+}
+
+document.body.onkeyup = function(e) {
+
+	if (e.keyCode === 49) {
+
+		btn1.style.boxShadow = "0px 2px 5px #555"
+		btn1.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 50) {
+
+		btn2.style.boxShadow = "0px 2px 5px #555"
+		btn2.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 51) {
+
+		btn3.style.boxShadow = "0px 2px 5px #555"
+		btn3.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 52) {
+
+		btn4.style.boxShadow = "0px 2px 5px #555"
+		btn4.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 53) {
+
+		btn5.style.boxShadow = "0px 2px 5px #555"
+		btn5.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 54) {
+
+		btn6.style.boxShadow = "0px 2px 5px #555"
+		btn6.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 55) {
+
+		btn7.style.boxShadow = "0px 2px 5px #555"
+		btn7.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 56) {
+
+		btn8.style.boxShadow = "0px 2px 5px #555"
+		btn8.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 57) {
+
+		btn9.style.boxShadow = "0px 2px 5px #555"
+		btn9.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 48) {
+
+		btn0.style.boxShadow = "0px 2px 5px #555"
+		btn0.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 8) {
+
+		btnBackspace.style.boxShadow = "0px 2px 5px #555"
+		btnBackspace.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 9) {
+
+		btnTab.style.boxShadow = "0px 2px 5px #555"
+		btnTab.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 81) {
+
+		btnQ.style.boxShadow = "0px 2px 5px #555"
+		btnQ.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 87) {
+
+		btnW.style.boxShadow = "0px 2px 5px #555"
+		btnW.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 69) {
+
+		btnE.style.boxShadow = "0px 2px 5px #555"
+		btnE.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 82) {
+
+		btnR.style.boxShadow = "0px 2px 5px #555"
+		btnR.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 84) {
+
+		btnT.style.boxShadow = "0px 2px 5px #555"
+		btnT.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 89) {
+
+		btnY.style.boxShadow = "0px 2px 5px #555"
+		btnY.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 85) {
+
+		btnU.style.boxShadow = "0px 2px 5px #555"
+		btnU.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 73) {
+
+		btnI.style.boxShadow = "0px 2px 5px #555"
+		btnI.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 79) {
+
+		btnO.style.boxShadow = "0px 2px 5px #555"
+		btnO.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 80) {
+
+		btnP.style.boxShadow = "0px 2px 5px #555"
+		btnP.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 65) {
+
+		btnA.style.boxShadow = "0px 2px 5px #555"
+		btnA.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 83) {
+
+		btnS.style.boxShadow = "0px 2px 5px #555"
+		btnS.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 68) {
+
+		btnD.style.boxShadow = "0px 2px 5px #555"
+		btnD.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 70) {
+
+		btnF.style.boxShadow = "0px 2px 5px #555"
+		btnF.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 71) {
+
+		btnG.style.boxShadow = "0px 2px 5px #555"
+		btnG.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 72) {
+
+		btnH.style.boxShadow = "0px 2px 5px #555"
+		btnH.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 74) {
+
+		btnJ.style.boxShadow = "0px 2px 5px #555"
+		btnJ.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 75) {
+
+		btnK.style.boxShadow = "0px 2px 5px #555"
+		btnK.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 76) {
+
+		btnL.style.boxShadow = "0px 2px 5px #555"
+		btnL.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 13) {
+
+		btnenter.style.boxShadow = "0px 2px 5px #555"
+		btnenter.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 16) {
+
+		btnShift.style.boxShadow = "0px 2px 5px #555"
+		btnShift.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 90) {
+
+		btnZ.style.boxShadow = "0px 2px 5px #555"
+		btnZ.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 88) {
+
+		btnX.style.boxShadow = "0px 2px 5px #555"
+		btnX.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 67) {
+
+		btnC.style.boxShadow = "0px 2px 5px #555"
+		btnC.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 86) {
+
+		btnV.style.boxShadow = "0px 2px 5px #555"
+		btnV.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 66) {
+
+		btnB.style.boxShadow = "0px 2px 5px #555"
+		btnB.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 78) {
+
+		btnN.style.boxShadow = "0px 2px 5px #555"
+		btnN.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 77) {
+
+		btnM.style.boxShadow = "0px 2px 5px #555"
+		btnM.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 17) {
+
+		btnCtrl.style.boxShadow = "0px 2px 5px #555"
+		btnCtrl.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 18) {
+
+		btnAlt.style.boxShadow = "0px 2px 5px #555"
+		btnAlt.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 32) {
+
+		btnspace.style.boxShadow = "0px 2px 5px #555"
+		btnspace.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 37) {
+
+		btnleft.style.boxShadow = "0px 2px 5px #555"
+		btnleft.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 38) {
+
+		btntop.style.boxShadow = "0px 2px 5px #555"
+		btntop.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 40) {
+
+		btndown.style.boxShadow = "0px 2px 5px #555"
+		btndown.style.backgroundColor = "lightgrey"
+
+	} else if (e.keyCode === 39) {
+
+		btnright.style.boxShadow = "0px 2px 5px #555"
+		btnright.style.backgroundColor = "lightgrey"
+
+	}
+
+}
+
+let a = false;
+
+$("#btn_difference").click(function() {
+
+	if (a === false) {
+
+		$(".keyboard").hide(250)
+		$(".mouse").toggle(250)
+		$("#btn_difference").html("Try keyboard")
+
+		a = true;
+
+	} else {
+
+		$(".keyboard").toggle(250)
+		$(".mouse").hide(250)
+		$("#btn_difference").html("Try mouse")
+
+		a = false;
+
+	}
+
+})
+
+$("#left_btn").mousedown(function() {
+
+	$(".left_btn").css("background-color", "#555")
+
+})
+
+$("#left_btn").mouseup(function() {
+
+	$(".left_btn").css("background-color", "#777")
+
+})
+
+$("#right_btn").mousedown(function() {
+
+	$(".right_btn").css("background-color", "#555")
+
+})
+
+$("#right_btn").mouseup(function() {
+
+	$(".right_btn").css("background-color", "#777")
+
+})
